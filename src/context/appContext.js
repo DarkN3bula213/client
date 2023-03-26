f2ebe36f7cbe0aa5d91d19216f52f02504bbd254
AA <a.ateeb87@gmail.com>
2023-03-26 14:23:49 +0500

    localstorage

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -9,22 +9,25 @@ import {
 } from "./actions";
 import axios from "axios";
 
+const user = localStorage.getItem('user') 
+const token = localStorage.getItem('token') 
+const userLocation = localStorage.getItem('location') 
+
 const initialState = {
   isLoading: false,
   showAlert: false,
   alertText: "",
   alertType: "",
-  user: null,
-  token: null,
-  userLocation: "",
-  jobLocation: "",
+  user: user ? JSON.parse(user) : null,
+  token: token,
+  userLocation: userLocation || "",
+  jobLocation: userLocation || "",
 };
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
-
   const displayAlert = () => {
     dispatch({ type: DISPLAY_ALERT });
     clearAlert();
@@ -35,21 +38,39 @@ const AppProvider = ({ children }) => {
       dispatch({ type: CLEAR_ALERT });
     }, 3000);
   };
+//local storage
+const addUserToLocalStorage = ({user,token, location}) => {
+  localStorage.setItem('user', JSON.stringify(user))
+  localStorage.setItem('token', token)
+  localStorage.setItem('location', location)
+}
+
+const removeUserFromLocalStorage = () => {
+  localStorage.removeItem('user')
+  localStorage.removeItem('token')
+  localStorage.removeItem('location')
+}
+
+
   const registerUser = async (currentuser) => {
     dispatch({ type: REGISTER_USER_BEGIN });
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/v1/users/register",
+        "/api/v1/auth/register",
         currentuser
       );
-      console.log(response);
+      // console.log(response);
       const { user, token, location } = response.data;
       dispatch({
         type: REGISTER_USER_SUCCESS,
-        payload: { user, token, location },
+        payload: {
+           user,
+          token, 
+          location },
       });
+      addUserToLocalStorage({user,token, location})
     } catch (error) {
-      console.log(error.response);
+      // console.log(error.response);
       dispatch({
         type: REGISTER_USER_ERROR,
         payload: { msg: error.response.data },
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,8 @@
-import { useState, UseEffect } from "react";
+import { useState, useEffect } from "react";
 import { Logo, FormRow, Alert } from "../Components";
 import Wrapper from "../assets/wrappers/RegisterPage";
 import { useAppContext } from "../context/appContext";
-
+import { useNavigate } from "react-router-dom";
 const initialState = {
   name: "",
   email: "",
@@ -10,55 +10,93 @@ const initialState = {
   isMember: true,
 };
 
+
 const Register = () => {
+  const navigate = useNavigate();
   const [values, setValues] = useState(initialState);
-  //global state and navigation
-  const {isLoading, showAlert, displayAlert, registerUser} = useAppContext() 
+  const { user, isLoading, showAlert, displayAlert, registerUser } =
+    useAppContext();
 
   //toggle between login and register
-    const toggleMember = () => {
+  const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
-    };
+  };
 
-// handle change in form
+  // handle change in form
   const handleChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
-    console.log(e.target);};
+    console.log(e.target);
+  };
 
-
-    // handle submit
+  // handle submit
   const onSubmit = (e) => {
     e.preventDefault();
     const { name, email, password, isMember } = values;
     if (!email || !password || (!isMember && !name)) {
-      displayAlert()
+      displayAlert();
       return;
     }
-    const currentUser = { name, email, password};
-    if(isMember){
-      console.log('Already a member');
-    }else{
+    const currentUser = { name, email, password };
+    if (isMember) {
+      console.log("Already a member");
+    } else {
       registerUser(currentUser);
     }
-    console.log(values);};
+    console.log(values);
+  };
 
-  return <Wrapper className="full-page">
-<form className="form" onSubmit={onSubmit}>
-    <Logo />
-    <h3>{values.isMember ? "Login" : "Register"}</h3>
+  // redirect to dashboard if user is logged in
+  useEffect(() => {
+    if(user){
+      setTimeout(() => {
+        navigate('/')        
+      }, 3000);
+    
+    }
+  }, [user, navigate])
+  
 
-    {showAlert && <Alert />}
-    {!values.isMember && (
-    <FormRow type="text" name="name" values={values.name} handleChange={handleChange} labelText="Name" />
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+
+        {showAlert && <Alert />}
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            values={values.name}
+            handleChange={handleChange}
+            labelText="Name"
+          />
         )}
-    <FormRow type="email" name="email" values={values.email} handleChange={handleChange} labelText="Email" />
-    <FormRow type="password" name="password" values={values.password} handleChange={handleChange} labelText="Password" />
-    <button type="submit" className="btn btn-block" disabled={isLoading}>Submit</button>
-    <p>
-    {values.isMember ? "Not a member?" : "Already a member?"}
-        <button type="button" onClick={toggleMember} className="member-btn">{values.isMember? 'Register':'Login'}</button>
-    </p>
-</form>
-  </Wrapper>;
+        <FormRow
+          type="email"
+          name="email"
+          values={values.email}
+          handleChange={handleChange}
+          labelText="Email"
+        />
+        <FormRow
+          type="password"
+          name="password"
+          values={values.password}
+          handleChange={handleChange}
+          labelText="Password"
+        />
+        <button type="submit" className="btn btn-block" disabled={isLoading}>
+          Submit
+        </button>
+        <p>
+          {values.isMember ? "Not a member?" : "Already a member?"}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
 };
 export default Register;
